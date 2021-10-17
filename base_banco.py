19d60a8d3dba00718494d728622550f3ed55281e
Lucas Honda <eliashondalucas@hotmail.com>
2021-10-17 02:56:58 -0300

    Fix query

--- base_banco.py
+++ base_banco.py
@@ -38,7 +38,7 @@ class Professor:
     def __init__(self, conn) -> None:
         query = """CREATE TABLE IF NOT EXISTS Professor (
     pessoa int PRIMARY KEY,
-    foreign key (pessoa) references Pessoa(cpf)
+    foreign key (pessoa) references Pessoa(cpf),
     codigo varchar (255) NOT NULL
     );"""
         self.mycursor = conn.mycursor
