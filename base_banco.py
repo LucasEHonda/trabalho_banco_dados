2a9d07edb36d88d99b4dc79c87da121b4a00992c
Lucas Honda <eliashondalucas@hotmail.com>
2021-10-17 02:46:20 -0300

    Fix indentation

--- base_banco.py
+++ base_banco.py
@@ -92,7 +92,7 @@ class Experiencia:
     );"""
 
 class ExperienciaAtendeModalidade:
-        def __init__(self,conn) -> None:
+    def __init__(self,conn) -> None:
         query = """CREATE TABLE IF NOT EXISTS ExperienciaAtendeModalidade (
     modalidade varchar(255) NOT NULL,
     foreign key (modalidade) references Modalidade(nome)
